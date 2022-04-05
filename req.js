75e3dfa7abc79e4c2d67ea79f8f9afdf1060a990
groupc22 <102436107+groupc22@users.noreply.github.com>
2022-04-05 12:05:53 +0100

    - added gallery page and updated URLS

--- req.js
+++ req.js
@@ -13,10 +13,25 @@ const IMG_URL = "https://image.tmdb.org/t/p/w500";
 
 const SEARCH_SINGLEM_URL = BASE_URL + "/search/movie?" + API_KEY;
 
+const TRENDING_TV_SHOWS = BASE_URL + "/trending/tv/week?" + API_KEY;
+
+const ANIMATION = BASE_URL + "/discover/movie?" + API_KEY + "&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=1&with_genres=16&with_watch_monetization_types=flatrate";
+
+const NIGERIAN_MOVIES = BASE_URL + "/search/movie?" + API_KEY + "&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&page=1&query=nigeria";
+
+// console.log(ANIMATION)
+
+
 // Getting popular movies
 getPopularMovies(POPULAR_MOVIES_URL);
 
 getUpcomingMovies(UPCOMING_URL);
+
+getTrendingTVShows(TRENDING_TV_SHOWS);
+
+getAnimationMovies(ANIMATION);
+
+getNigerianMovies(NIGERIAN_MOVIES);
 
 //getting popular movies
 function getPopularMovies(url) {
@@ -29,13 +44,41 @@ function getPopularMovies(url) {
 }
 
 // getting upcoming movies
-//getting the movies
 function getUpcomingMovies(url) {
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
       // console.table(data.results);
       showUpcomingMovies(data.results);
+    });
+}
+
+// getting trending tv Shows
+function getTrendingTVShows(url) {
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      // console.table(data.results);
+      showTrendingTVShows(data.results);
+    });
+}
+
+// getting animation movies 
+function getAnimationMovies(url) {
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      // console.table(data.results);
+      showAnimationMovies(data.results);
+    });
+}
+
+function getNigerianMovies(url) {
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      // console.table(data.results);
+      showNigerianMovies(data.results);
     });
 }
 
@@ -113,10 +156,125 @@ function showUpcomingMovies(movies) {
 }
 
 
-function movieSelected(title){
-  sessionStorage.setItem('movieTitle',title);
-  window.location = '/src/movie.html';
-  return false;
+// show trending tv shows
+function showTrendingTVShows(movies) {
+  movies.forEach((movie) => {
+    // getting the movies
+    const movieTitle = movie.name;
+    const movieRating = movie.vote_average;
+    const movieGenreID = movie.genre_ids[0];
+    const moviePoster = IMG_URL + movie.poster_path;
+
+    // embeding data in html page
+    const movieGallery = document.querySelector(
+      ".ld-video-data-contain-trending-tv"
+    );
+
+    const movieBox = document.createElement("div");
+    movieBox.classList.add("ld-video-item");
+
+    movieBox.innerHTML = `
+      <div class="ld-video-cover">
+          <img
+          src="${moviePoster}"
+          alt="${movieTitle}" />
+          <a href="" class="ld-video-play-btn"></a>
+          <span class="ld-video-rating">${movieRating}</span>
+      </div>
+      <div class="ld-video-content">
+          <h3 class="ld-video-title">${movieTitle}</h3>
+          <span class="ld-video-category"></span>
+      </div>
+      `;
+
+    movieGallery.appendChild(movieBox);
+  });
 }
+
+function showAnimationMovies(movies){
+
+  for (let i = 0; i < 10; i++) {
+
+
+  // movies.forEach((movie) => {
+    // getting the movies
+    const movieTitle = movies[i].title;
+    const movieRating = movies[i].vote_average;
+    const movieGenreID = movies[i].genre_ids[0];
+    const moviePoster = IMG_URL + movies[i].poster_path;
+
+    // embeding data in html page
+    const movieGallery = document.querySelector(
+      ".ld-video-data-contain-animation"
+    );
+
+    const movieBox = document.createElement("div");
+    movieBox.classList.add("ld-video-item");
+
+    movieBox.innerHTML = `
+      <div class="ld-video-cover">
+          <img
+          src="${moviePoster}"
+          alt="${movieTitle}" />
+          <a href="" class="ld-video-play-btn"></a>
+          <span class="ld-video-rating">${movieRating}</span>
+      </div>
+      <div class="ld-video-content">
+          <h3 class="ld-video-title">${movieTitle}</h3>
+          <span class="ld-video-category"></span>
+      </div>
+      `;
+
+    movieGallery.appendChild(movieBox);
+  // });
+}
+}
+
+function showNigerianMovies(movies){
+
+  // for (let i = 0; i < 10; i++) {
+
+
+  movies.forEach((movie) => {
+    // getting the movies
+    const movieTitle = movie.title;
+    const movieRating = movie.vote_average;
+    const movieGenreID = movie.genre_ids[0];
+    const moviePoster = IMG_URL + movie.poster_path;
+
+    // embeding data in html page
+    const movieGallery = document.querySelector(
+      ".ld-video-data-contain-nigeria"
+    );
+
+    const movieBox = document.createElement("div");
+    movieBox.classList.add("ld-video-item");
+
+    if ( movie.poster_path != null) {
+
+    movieBox.innerHTML = `
+      <div class="ld-video-cover">
+          <img
+          src="${moviePoster}"
+          alt="${movieTitle}" />
+          <a href="" class="ld-video-play-btn"></a>
+          <span class="ld-video-rating">${movieRating}</span>
+      </div>
+      <div class="ld-video-content">
+          <h3 class="ld-video-title">${movieTitle}</h3>
+          <span class="ld-video-category"></span>
+      </div>
+      `;
+
+    movieGallery.appendChild(movieBox);}
+  });
+// }
+}
+
+// function movieSelected(title){
+//   sessionStorage.setItem('movieTitle',title);
+//   window.location = '/src/movie.html';
+//   return false;
+// }
 
 
